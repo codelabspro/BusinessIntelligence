f4f7c1f59a87d75b62c54cfcd7add0f0f40132d9
Code SDK <codesdktutorials@gmail.com>
2015-12-13 18:40:10 -0500

    Added targetdevices to to_s method

--- BusinessIntelligence/application.rb
+++ BusinessIntelligence/application.rb
@@ -26,9 +26,9 @@ class Application
     def to_s(format='full')
         case format
         when 'full'
-            to_string = "#{title} #{description} #{version}"
+            to_string = "#{title} #{description} #{version} : #{targetdevices}"
         when 'partial'
-            to_string = "#{title} #{description}"
+            to_string = "#{title} : #{targetdevices}"
         end
         to_string
     end
@@ -43,6 +43,7 @@ firstApp.version = "1.0"
 firstApp.add_targetdevice("Chrome", "Google's Web Brower")
 firstApp.add_targetdevice("Firefox", "Firefox Web Brower")
 puts firstApp.to_s('full')
+# puts firstApp.inspect
 
 secondApp = Application.new
 secondApp.title = "Second App"
@@ -50,3 +51,4 @@ secondApp.description = "Game for TV"
 secondApp.version = "2.0"
 secondApp.add_targetdevice("Apple TV", "Apple's 3rd generation Apple TV")
 puts secondApp.to_s('full')
+# puts secondApp.inspect
